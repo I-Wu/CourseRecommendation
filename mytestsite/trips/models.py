9eb1fb6b930893ebaf3072ffb6f3d3964a7366bc
gau820827 <gau820827@gmail.com>
2018-03-24 13:22:51 -0400

    [gau820827] Add recommend func in models.py

--- mytestsite/trips/models.py
+++ mytestsite/trips/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Course():
+
+    data = {}
+
+    def __init__(self, cid, title, recommendation):
+        """ Initialize a Course recommendation """
+        self.id = cid
+        self.title = title
+        self.recommendation = recommendation
+
+    @staticmethod
+    def get_recommend(course_name):
+        return Course.data[course_name].recommendation
--- mytestsite/trips/views.py
+++ mytestsite/trips/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Course
+
 from datetime import datetime
 
 def home(request):
 	#return HttpResponse("Hello World!")
 	return render(request, 'hello_world.html', {
-        'current_time': str(datetime.now()),
+           'current_time': str(datetime.now()),
     })
+
 def find_similarity(request):
     if 'keyword' in request.GET:
-    	#list a = models.function(request.GET['keyword'])
-    	#return render()
+        recommend = Course.get_recommend((request.GET['keyword']))
+        #return render()
         return HttpResponse('Welcome!~' + request.GET['keyword'])
     else:
         return HttpResponse('not allowed')
